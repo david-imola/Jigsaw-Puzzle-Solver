990861c6aede5ffae1b0795a97f409659e33f776
David Imola <imola.david72@gmail.com>
2018-09-04 22:32:56 -0600

    Made a few changes to the solve module

--- PuzzleSolve/core/solve.py
+++ PuzzleSolve/core/solve.py
@@ -2,8 +2,18 @@ from PIL import Image
 import numpy as np
 
 
-def _mahaDistanceSymetric():
-    pass
+
+
+
+def _addRandomNoise(array):
+    np.random.seed(0) # We actually want consistent noise
+    noisey = np.random.randint(0, 2, size=(9, 3))
+    return np.concatenate(array, noisey)
+
+def _compatMeasure(gradArray):
+    mean = np.mean(gradArray, axis=1)
+
+
 
 
 class _Piece:
@@ -12,18 +22,23 @@ class _Piece:
         x0, y0 = j * pieceLen, i * pieceLen
         x1, y1 = x0 + pieceLen, y0 + pieceLen
         pieceArray = imgArray[x0:x1, y0:y1]
-        self.grad_left = pieceArray[:, 1] - pieceArray[:, 0]
-        self.grad_right = pieceArray[:, pieceLen - 2]\
+        self.grad = []  # 0 for top, 1 for right, 2 for bottom, 3 for left
+        self.grad[0] = pieceArray[:, 1] - pieceArray[:, 0]
+        self.grad[1] = pieceArray[:, pieceLen - 2]\
             - pieceArray[:, pieceLen - 1]
-        self.grad_bottom = pieceArray[pieceArray - 2, :]\
+        self.grad[2] = pieceArray[pieceArray - 2, :]\
             - pieceArray[pieceArray - 1, :]
-        self.grad_top = pieceArray[1, :] - pieceArray[0, :]
+        self.grad[3] = pieceArray[1, :] - pieceArray[0, :]
+
+        for i in range(4):
+            self.grad[i] = np.reshape(self.grad[i], newshape=(pieceLen, 3))
 
 
 class _Edge:
     def __init__(self, piece0, piece1):
-        self.piece0 = piece0
-        self.piece1 = piece1
+        self.pieces = []
+        self.piece[0] = piece0
+        self.piece[1] = piece1
         # TODO implement cost function here, so that it can determine the orientation
 
 
@@ -38,4 +53,6 @@ class JigsawTree:
         self.pieces = []
         for i in range(rows):
             for j in range(cols):
-                self.pieces.append(_Piece(imArray, i, j, pieceLen))
+                newPiece = _Piece(imArray, i, j, pieceLen)
+                for piece in pieces
+                self.pieces.append()
